e6456f2d57cfc903ce04d3be4b02172db5bd66dd
Shannon Grant <grant.shannon90@gmail.com>
2020-10-06 16:23:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ require 'pry'
 def reverse_each_word (sentence)
   words_array = sentence.split
   new_array = []
-  new_array = words_array.collect {|word| word.reverse}
+  new_array = words_array.each {|word| word.reverse}
   new_array.join (" ")
 end
