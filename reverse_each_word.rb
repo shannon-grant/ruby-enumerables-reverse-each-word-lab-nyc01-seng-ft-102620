d0aaa6a8a183dab80e51d3ef2edafd10184b1de6
Shannon Grant <grant.shannon90@gmail.com>
2020-10-06 16:22:04 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+require 'pry'
+
+def reverse_each_word (sentence)
+  words_array = sentence.split
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse}
+  new_array.join (" ")
+end
