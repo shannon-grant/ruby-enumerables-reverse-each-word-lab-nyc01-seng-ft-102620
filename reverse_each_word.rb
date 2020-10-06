52ead15d5caaaf48618fab0303aaf3fa013fb039
Shannon Grant <grant.shannon90@gmail.com>
2020-10-06 16:24:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 require 'pry'
 
 def reverse_each_word (sentence)
-  words_array = sentence.split
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse}
-  new_array.join (" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
